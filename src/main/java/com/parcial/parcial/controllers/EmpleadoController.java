93c4b3d58ff9b02714065c27d25937d6733efd3e
ThePixels21 <santigamer521@gmail.com>
2023-03-07 08:31:30 -0500

    first commit

---
+++ src/main/java/com/parcial/parcial/controllers/EmpleadoController.java
@@ -0,0 +1,20 @@
+package com.parcial.parcial.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.parcial.parcial.models.Empleado;
+
+@Controller
+@RequestMapping("/empleado")
+public class EmpleadoController {
+    
+    @GetMapping({"/", ""})
+    public String formulario(Model model) {
+        model.addAttribute("titulo", "Formulario");
+        model.addAttribute("empelado", new Empleado());
+        return "empleado/index";
+    }
+}
---
+++ src/main/java/com/parcial/parcial/models/Empleado.java
@@ -0,0 +1,73 @@
+package com.parcial.parcial.models;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class Empleado {
+    @NotEmpty
+    private String id;
+    @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String apellido;
+    @NotEmpty
+    @Size(min=6, max=150)
+    private String correo;
+    @NotEmpty
+    @Size(min=5, max=100)
+    private String direccion;
+    @NotEmpty
+    @Size(min=10, max=13)
+    private String telefono;
+
+    public Empleado() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+}
